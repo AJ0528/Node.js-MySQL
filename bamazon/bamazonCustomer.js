c446b896fb3f98774b89c089db082e19e746abef
Alfredo <puertorocat@gmail.com>
2019-04-02 19:21:07 -0400

    change to server

--- bamazon/bamazonCustomer.js
+++ bamazon/bamazonCustomer.js
@@ -1,43 +1,75 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 3306 ,
+  port: 8889 ,
 
   // Your username
   user: "root",
 
   // Your password
   password: "root",
-  database: "bamazon"
+  database: "bamazon_db"
 });
+
 connection.connect(function(err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     start();
   });
 
-  function start() {
-    inquirer
-      .prompt({
-        name: "bamazon_products",
+function start() {
+  connection.query("SELECT * FROM products", function(err, results) {
+  if (err) throw err;
+
+      var table = new Table({
+      head: ['ID','Product Name','Department Name', 'Stock','Price']
+    , colWidths: [5,18,20,12,12]
+      });
+
+   
+    
+    for (var i = 0; i < results.length; i++) {
+      table.push(
+        [results[i].id, results[i].product_name, results[i].deparment_name, results[i].stock_quantity, results[i].price]
+       
+        
+      )
+      
+    }
+   console.log(table.toString());
+   purchase()
+    });
+
+  
+  // function to handle posting new items up for auction
+function purchase() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "item",
         type: "input",
-        message: "Enter ID of the product they would like to buy?",
-        choices: ["1", "2", "3", "4", "5", "6", "7","8","9","10"]
-      })
+        message: "Enter the ID of what product you like to buy?"
+      },
+      {
+        name: "quanity",
+        type: "input",
+        message: " how many units of the product they would like to buy?"
+      },
+    ])
       .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.bamazon_products  === "1", "2", "3", "4", "5", "6", "7","8","9","10") {
-          bamazon_products();
-        }
-  //       else if(answer.bamazon_products === "") {
-  //         bidAuctio();
-  //       } else{
-  //         connection.end();
-  //       }
-  //     });
-  // }
+        // when finished prompting, insert a new item into the db with that info
+      console.log(answer.item);
+      console.log(answer.quanity);
+      })
+    }
+  
+  }
+  connection.query()
+
